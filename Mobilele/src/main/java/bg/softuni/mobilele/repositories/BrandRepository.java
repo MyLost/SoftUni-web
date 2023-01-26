3e15658f9a994b8f37863baa6d7817ea88204f82
darthReader <ddimitrov10@gmail.com>
2023-01-26 09:53:48 +0200

    WIP

---
+++ Mobilele/src/main/java/bg/softuni/mobilele/repositories/BrandRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.mobilele.repositories;
+
+import bg.softuni.mobilele.domain.entities.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, String> {
+}
