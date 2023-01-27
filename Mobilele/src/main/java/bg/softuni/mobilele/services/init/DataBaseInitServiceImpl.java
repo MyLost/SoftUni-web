0cdccd01c6deb5ed9fefc05c864543c16c841e85
darthReader <ddimitrov10@gmail.com>
2023-01-27 09:47:48 +0200

    WIP

---
+++ Mobilele/src/main/java/bg/softuni/mobilele/services/init/DataBaseInitServiceImpl.java
@@ -0,0 +1,33 @@
+package bg.softuni.mobilele.services.init;
+
+import bg.softuni.mobilele.services.role.UserRoleService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataBaseInitServiceImpl implements DataBaseInitService {
+    private final UserRoleService roleService;
+
+    @Autowired
+    public DataBaseInitServiceImpl(UserRoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        dbInit();
+    }
+
+    @Override
+    public void dbInit() {
+        if (isDbInit()) {
+            this.roleService.dbInit();
+        }
+    }
+
+    @Override
+    public boolean isDbInit() {
+        return this.roleService.isDbInit();
+    }
+}
