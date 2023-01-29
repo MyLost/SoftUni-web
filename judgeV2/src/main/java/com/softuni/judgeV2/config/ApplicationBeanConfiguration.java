aa93e257e4cf3e8c0219a197039ae2997fb0d00c
darthReader <ddimitrov10@gmail.com>
2023-01-29 10:00:46 +0200

    initial commit

---
+++ judgeV2/src/main/java/com/softuni/judgeV2/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,9 @@
+package com.softuni.judgeV2.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class ApplicationBeanConfiguration {
+
+}
