4617cbb1aa208cd73a195027586b96c709d2eb58
darthReader <ddimitrov10@gmail.com>
2023-01-29 10:01:08 +0200

    WIP

---
+++ judgeV2/src/main/java/com/softuni/judgeV2/models/entities/ExerciseEntity.java
@@ -0,0 +1,30 @@
+package com.softuni.judgeV2.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="exercises")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExerciseEntity extends BaseEntity {
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @Column(name="started_on")
+    private LocalDateTime startedOn;
+
+    @Column(name="due_date")
+    private LocalDateTime dueDate;
+
+}
