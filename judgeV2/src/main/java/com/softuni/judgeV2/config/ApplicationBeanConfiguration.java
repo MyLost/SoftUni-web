28b888f6c51a7761f65dbd3ac034076050c494f4
darthReader <ddimitrov10@gmail.com>
2023-01-30 07:48:45 +0200

    WIP: enhancement

--- judgeV2/src/main/java/com/softuni/judgeV2/config/ApplicationBeanConfiguration.java
+++ judgeV2/src/main/java/com/softuni/judgeV2/config/ApplicationBeanConfiguration.java
@@ -1,5 +1,7 @@
 package com.softuni.judgeV2.config;
 
+import com.softuni.judgeV2.mappers.UserMapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 
