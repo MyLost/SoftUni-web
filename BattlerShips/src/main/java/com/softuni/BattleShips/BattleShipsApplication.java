c6a5f93350a8096db4c1ffc4c60651f11b2b4120
darthReader <ddimitrov10@gmail.com>
2023-02-10 08:35:03 +0200

    project enhancement

---
+++ BattlerShips/src/main/java/com/softuni/BattleShips/BattleShipsApplication.java
@@ -0,0 +1,14 @@
+package com.softuni.BattleShips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleShipsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BattleShipsApplication.class, args);
+	}
+
+}
