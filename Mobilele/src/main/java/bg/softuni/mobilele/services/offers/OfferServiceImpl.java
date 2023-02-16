5c64976504fbba3706a7c901c2454828446065d9
darthReader <ddimitrov10@gmail.com>
2023-02-16 07:20:32 +0200

    project enhancement

--- Mobilele/src/main/java/bg/softuni/mobilele/services/offers/OfferServiceImpl.java
+++ Mobilele/src/main/java/bg/softuni/mobilele/services/offers/OfferServiceImpl.java
@@ -1,13 +1,12 @@
 package bg.softuni.mobilele.services.offers;
 
-import bg.softuni.mobilele.services.init.DataBaseInitService;
+import bg.softuni.mobilele.services.init.DataBaseInitServiceService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OfferServiceImpl implements OfferService, DataBaseInitService {
+public class OfferServiceImpl implements OfferService, DataBaseInitServiceService {
     @Override
     public void dbInit() {
-
     }
 
     @Override
