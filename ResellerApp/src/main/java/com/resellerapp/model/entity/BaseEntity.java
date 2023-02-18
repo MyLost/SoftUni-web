a66136253dfc8cd32588306e54644e0b6bd17f4a
darthReader <ddimitrov10@gmail.com>
2023-02-18 20:06:12 +0200

    project enhancement

---
+++ ResellerApp/src/main/java/com/resellerapp/model/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.resellerapp.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
