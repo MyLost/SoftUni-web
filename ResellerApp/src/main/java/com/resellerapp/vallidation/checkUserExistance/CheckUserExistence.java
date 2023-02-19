a6e7ac56d6edc425930793bd1ff7037d5d9f6750
darthReader <ddimitrov10@gmail.com>
2023-02-19 11:19:50 +0200

    project enhancement

---
+++ ResellerApp/src/main/java/com/resellerapp/vallidation/checkUserExistance/CheckUserExistence.java
@@ -0,0 +1,21 @@
+package com.resellerapp.vallidation.checkUserExistance;
+
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Constraint(validatedBy = UserLoginValidator.class)
+public @interface CheckUserExistence {
+    String message() default "Invalid user";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
