9abe75a296da0f8b690a94cf30932a2446a00eb4
darthReader <ddimitrov10@gmail.com>
2023-02-23 09:21:10 +0200

    project enhancement

---
+++ ResellerApp/src/main/java/com/resellerapp/repository/OfferRepository.java
@@ -0,0 +1,16 @@
+package com.resellerapp.repository;
+
+import com.resellerapp.model.entity.OfferEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface OfferRepository extends JpaRepository<OfferEntity, Long> {
+    Optional<Set<OfferEntity>> findAllByUserId(Long id);
+
+    Optional<Set<OfferEntity>> findAllByUserIdNot(Long id);
+}
