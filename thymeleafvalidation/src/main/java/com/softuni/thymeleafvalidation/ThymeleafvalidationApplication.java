c0d63cc330e04a20ce002559eef8503acb26b535
darthReader <ddimitrov10@gmail.com>
2023-03-03 10:23:46 +0200

    project enhancement

---
+++ thymeleafvalidation/src/main/java/com/softuni/thymeleafvalidation/ThymeleafvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.thymeleafvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafvalidationApplication.class, args);
+	}
+
+}
