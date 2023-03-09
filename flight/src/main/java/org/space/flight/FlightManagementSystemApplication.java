d60a60303d12638a8e7520d633d59358c6998c0c
darthReader <ddimitrov10@gmail.com>
2023-03-09 12:11:56 +0200

    project enhancement

---
+++ flight/src/main/java/org/space/flight/FlightManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.space.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagementSystemApplication.class, args);
+	}
+
+}
