aed724a52ba5a9d8c24f8210a4b708ebbd3fea10
darthReader <ddimitrov10@gmail.com>
2023-03-10 08:52:58 +0200

    project enhancement

---
+++ flight/src/test/java/org/space/flight/FlightManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.space.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
