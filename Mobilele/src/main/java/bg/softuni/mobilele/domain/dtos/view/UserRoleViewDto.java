c1e2fc43644c89c02c84fb96ea1631b623e0b397
darthReader <ddimitrov10@gmail.com>
2023-04-09 09:29:12 +0300

    project enhancement

---
+++ Mobilele/src/main/java/bg/softuni/mobilele/domain/dtos/view/UserRoleViewDto.java
@@ -0,0 +1,14 @@
+package bg.softuni.mobilele.domain.dtos.view;
+
+public class UserRoleViewDto {
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public UserRoleViewDto setRole(String role) {
+        this.role = role;
+        return this;
+    }
+}
