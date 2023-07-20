6c06cab1623237d2a5b863f2d051b0a618a5c442
darthReader <ddimitrov10@gmail.com>
2023-07-20 09:28:12 +0300

    add dto's

---
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/model/dto/AuthorDto.java
@@ -0,0 +1,15 @@
+package com.softuni.book.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class AuthorDto {
+
+    private String name;
+
+}
