c36513b5e1d5cc7a336361c2c03f1f140aafdf5c
darthReader <ddimitrov10@gmail.com>
2023-07-21 11:13:27 +0300

    add user to security layer

--- BOOKS/book-rest-server/src/main/java/com/softuni/book/service/ApplicationUserDetailsService.java
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/service/ApplicationUserDetailsService.java
@@ -1,10 +1,14 @@
 package com.softuni.book.service;
 
+import com.softuni.book.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class ApplicationUserDetailsService implements UserDetailsService {
+    public ApplicationUserDetailsService(UserRepository userRepository) {
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
