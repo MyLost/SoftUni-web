4ef94494786f38fc11676cf36c6458135ce2242c
darthReader <ddimitrov10@gmail.com>
2023-07-24 20:51:42 +0300

    add entities

--- BOOKS/book-rest-server/src/main/java/com/softuni/book/model/entity/BookEntity.java
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/model/entity/BookEntity.java
@@ -1,7 +1,22 @@
 package com.softuni.book.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "books")
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BookEntity extends BaseEntity {
+
+    private String title;
+    private String isbn;
+    @ManyToOne
+    private AuthorEntity author;
 }
