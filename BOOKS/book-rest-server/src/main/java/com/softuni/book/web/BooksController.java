518064834520f181df05d8c038dd7032b881c34c
darthReader <ddimitrov10@gmail.com>
2023-07-29 17:14:54 +0300

    add controllers, services, dtos and other stuff

---
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/web/BooksController.java
@@ -0,0 +1,71 @@
+package com.softuni.book.web;
+
+import com.softuni.book.model.dtos.BookDto;
+import com.softuni.book.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/books")
+public class BooksController {
+
+    private final BookService bookService;
+
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getAllBook() {
+        return ResponseEntity.
+                ok(bookService.getAllBooks());
+    }
+
+    @Operation(summary = "Get book by the given ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "If the book was discovered.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = BookDto.class))}),
+                    @ApiResponse(responseCode = "400", description = "If the ID was incorrect."),
+                    @ApiResponse(responseCode = "404", description = "If the book was not found.")
+            }
+    )
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDto> getBookById(@PathVariable("id") Long bookId) {
+        Optional<BookDto> theBook = bookService.findBookById(bookId);
+
+        return
+                theBook.
+                        map(ResponseEntity::ok).
+                        orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BookDto> deleteBookById(@PathVariable("id") Long bookId) {
+        bookService.deleteById(bookId);
+
+        return ResponseEntity.
+                noContent().
+                build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<BookDto> createBook(@RequestBody BookDto newBook,
+                                              UriComponentsBuilder uriComponentsBuilder) {
+        long newBookId = bookService.createBook(newBook);
+
+        return ResponseEntity.created(uriComponentsBuilder.
+                        path("/api/books/{id}").build(newBookId)).
+                build();
+    }
+
+}
