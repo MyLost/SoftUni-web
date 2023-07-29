a7ce24180c3267f079330e3b65722e22477db120
darthReader <ddimitrov10@gmail.com>
2023-07-29 17:15:17 +0300

    add controllers, services, dtos and other stuff

---
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/model/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package com.softuni.book.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class UserEntity extends BaseEntity {
+
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<UserRoleEntity> roles = new ArrayList<>();
+}
