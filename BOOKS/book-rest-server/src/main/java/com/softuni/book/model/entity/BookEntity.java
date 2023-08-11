8680c2d9f312b33ea2826e68735e8cbf815092e5
darthReader <ddimitrov10@gmail.com>
2023-08-11 09:01:18 +0300

    WIP

--- BOOKS/book-rest-server/src/main/java/com/softuni/book/model/entity/BookEntity.java
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/model/entity/BookEntity.java
@@ -1,19 +1,29 @@
 package com.softuni.book.model.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "books")
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class BookEntity extends BaseEntity {
 
     private String title;
-    private String isbn;
+    private String isbn13;
     @ManyToOne
-    private AuthorEntity author;
+    private BookLanguageEntity language;
+    private Long pages;
+    private LocalDate publicationDate;
+    @ManyToOne
+    private PublisherEntity publisher;
+    @ManyToMany
+    private List<AuthorEntity> authors;
+    @OneToMany
+    private List<OrderLineEntity> orderLines;
+
 }
