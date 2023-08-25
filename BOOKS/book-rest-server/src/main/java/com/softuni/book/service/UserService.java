92d753aa97d0f07544923677640d0d219cfb8df1
darthReader <ddimitrov10@gmail.com>
2023-08-25 07:39:44 +0300

    WIP

--- BOOKS/book-rest-server/src/main/java/com/softuni/book/service/UserService.java
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/service/UserService.java
@@ -1,18 +1,12 @@
 package com.softuni.book.service;
 
 import com.softuni.book.model.dtos.UserRegistrationDto;
+//import com.softuni.book.model.entity.UserEntity;
+//import com.softuni.book.repository.UserRepository;
 import com.softuni.book.model.entity.UserEntity;
 import com.softuni.book.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolderStrategy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
@@ -21,26 +15,26 @@ import java.util.function.Consumer;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final SecurityContextRepository securityContextRepository;
-    private final UserDetailsService userDetailsService;
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, SecurityContextRepository securityContextRepository, UserDetailsService userDetailsService) {
+//    private final PasswordEncoder passwordEncoder;
+//    private final SecurityContextRepository securityContextRepository;
+//    private final UserDetailsService userDetailsService;
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.securityContextRepository = securityContextRepository;
-        this.userDetailsService = userDetailsService;
+//        this.passwordEncoder = passwordEncoder;
+//        this.securityContextRepository = securityContextRepository;
+//        this.userDetailsService = userDetailsService;
     }
 
-    public void RegisterUser(UserRegistrationDto registrationDto, Consumer<Authentication> successfulLoginProcessor) {
-        UserEntity userEntity = UserEntity.builder().build();
-
-        userRepository.save(userEntity);
-
-        var userDetails = userDetailsService.loadUserByUsername(registrationDto.getEmail());
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-            userDetails, userDetails.getPassword(), userDetails.getAuthorities()
-        );
-
-        successfulLoginProcessor.accept(authentication);
-    }
+//    public void RegisterUser(UserRegistrationDto registrationDto, Consumer<Authentication> successfulLoginProcessor) {
+//        UserEntity userEntity = UserEntity.builder().build();
+//
+//        userRepository.save(userEntity);
+//
+//        var userDetails = userDetailsService.loadUserByUsername(registrationDto.getEmail());
+//        Authentication authentication = new UsernamePasswordAuthenticationToken(
+//            userDetails, userDetails.getPassword(), userDetails.getAuthorities()
+//        );
+//
+//        successfulLoginProcessor.accept(authentication);
+//    }
 }
