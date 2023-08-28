625580c330da84bde9a985fa197df60545b12ccf
darthReader <ddimitrov10@gmail.com>
2023-08-28 08:12:59 +0300

    WIP

--- BOOKS/book-rest-server/src/main/java/com/softuni/book/security/config/SecurityConfiguration.java
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/security/config/SecurityConfiguration.java
@@ -6,50 +6,42 @@ import com.softuni.book.service.ApplicationUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.context.DelegatingSecurityContextRepository;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextRepository;
+
 
 @Configuration
+//@EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, SecurityContextRepository securityContextRepository) throws Exception {
-        http
-            .authorizeHttpRequests((authorizeHttpRequests) ->
-                // defines which pages will be authorized
-                authorizeHttpRequests
-                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                    .requestMatchers("/", "/users/login", "/users/register").permitAll()
-                    .requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name())
-                    .requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name())
-                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http, SecurityContextRepository securityContextRepository) throws Exception {
+//        http
+//            .authorizeHttpRequests((authorizeHttpRequests) ->
+//                // defines which pages will be authorized
+//                authorizeHttpRequests
+//                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+//                    .requestMatchers("/", "/users/login", "/users/register").permitAll()
+//                    .requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name())
+//                    .requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name())
+//                .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults());
+//        return http.build();
+//    }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(UserRepository userRepository) {
+//        return new ApplicationUserDetailsService(userRepository);
+//    }
 
-    @Bean
-    public UserDetailsService userDetailsService(UserRepository userRepository) {
-        return new ApplicationUserDetailsService(userRepository);
-    }
-
-    @Bean
-    public SecurityContextRepository securityContextRepository () {
-        return new DelegatingSecurityContextRepository(
-                new RequestAttributeSecurityContextRepository(),
-                new HttpSessionSecurityContextRepository()
-        );
-    }
+//    @Bean
+//    public SecurityContextRepository securityContextRepository () {
+//        return new DelegatingSecurityContextRepository(
+//                new RequestAttributeSecurityContextRepository(),
+//                new HttpSessionSecurityContextRepository()
+//        );
+//    }
 }
