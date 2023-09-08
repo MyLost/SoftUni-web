d52dfa718bcac3af5d145524f04b5d9d8a7ab434
darthReader <ddimitrov10@gmail.com>
2023-09-08 20:42:28 +0300

    WIP

--- BOOKS/book-rest-server/src/main/java/com/softuni/book/security/config/ApplicationConfiguration.java
+++ BOOKS/book-rest-server/src/main/java/com/softuni/book/security/config/ApplicationConfiguration.java
@@ -1,12 +1,21 @@
 package com.softuni.book.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing
 public class ApplicationConfiguration implements WebMvcConfigurer {
 
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new ApplicationAuditorAware();
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
